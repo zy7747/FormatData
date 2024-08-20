6fd209718522e9a03c1dfb4b233199fde1d29582
zy <461768522@qq.com>
2024-08-20 19:53:55 +0800

    1.0.1

--- formatData/toHump.js
+++ formatData/toHump.js
@@ -1,4 +1,9 @@
-// 下划线转换驼峰
+/**
+ * 将字符串转换为驼峰命名法
+ *
+ * @param name 待转换的字符串
+ * @returns 转换后的驼峰命名法字符串
+ */
 function toHump(name) {
   return name.replace(/\_(\w)/g, function (_all, letter) {
     return letter.toUpperCase();
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const toHump = require("./formatData/toHump.js");
+const handleTree = require("./formatData/handleTree.js");
 
 const FormatDataMap = {
   toHump,
+  handleTree,
 };
 
 module.exports = FormatDataMap;
